38b5f3fccca55a6a98f73c099d3e470ec4e8af67
Muhammad Awan <muhammadawanmoon@gmail.com>
2025-12-27 22:06:22 +0000

    ne bash  file

--- mysite/build.sh
+++ mysite/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # build_files.sh
+# Install dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+
+# Create the static folder and move files there
+python3.12 manage.py collectstatic --noinput --clear
